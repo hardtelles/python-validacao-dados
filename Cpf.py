a6fd8f09587a1ad53941388000a4fbc015c945ae
hardtelles <hardtelles@gmail.com>
2023-02-28 08:03:57 -0300

    utilizando um pacote pronto para validar CPF

--- Cpf.py
+++ Cpf.py
@@ -1,3 +1,6 @@
+from validate_docbr import CPF
+
+
 class Cpf:
     def __init__(self, documento):
         documento = str(documento)
@@ -6,27 +9,17 @@ class Cpf:
         else:
             raise ValueError("CPF Inválido!!")
 
-
-    def cpf_eh_valido(selfself, documento):
+    def cpf_eh_valido(self, documento):
         if len(documento) == 11:
-            return True
+            validador = CPF()
+
+            return validador.validate(documento)
         else:
-            return False
+            raise ValueError("Quantidade de digitos inválida!!")
 
     def __str__(self):
         return self.format_cpf()
 
     def format_cpf(self):
-        fatia_um = self.cpf[:3]
-        fatia_dois = self.cpf[3:6]
-        fatia_tres = self.cpf[6:9]
-        fatia_quatro = self.cpf[9:]
-
-        return(
-            "{}.{}.{}-{}".format(
-                fatia_um,
-                fatia_dois,
-                fatia_tres,
-                fatia_quatro
-            )
-        )
+        mascara = CPF()
+        return mascara.mask(self.cpf)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Cpf import Cpf
+from validate_docbr import CPF
 cpf = "29234725867"
 objeto_cpf = Cpf(cpf)
 
 
 print(objeto_cpf)
+
 
